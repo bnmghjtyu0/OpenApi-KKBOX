9393dc1c38ed4045f6deb3f15f0791c8514bdb56
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2018-01-16 23:37:12 +0800

    dev0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Auth } from "@kkbox/kkbox-js-sdk";
+import { Api } from "@kkbox/kkbox-js-sdk";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "app";
+  ngOnInit() {
+    const auth = new Auth(
+      "60016913f613f55c92464838fe168e81",
+      "c0b101bf141418eb0cdbb992727f4ade"
+    );
+    auth.clientCredentialsFlow.fetchAccessToken().then(response => {
+      const access_token = response.data.access_token;
+      const api = new Api(access_token);
+      api.searchFetcher
+        .setSearchCriteria("五月天 派對動物", "track")
+        .fetchSearchResult()
+        .then(response => {
+          console.log(response.data);
+        });
+    });
+  }
 }
