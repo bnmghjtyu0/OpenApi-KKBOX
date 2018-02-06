9a36bffa8a6d4ac915ddf9f6a3a690191bc75bdc
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2018-02-06 14:17:46 +0800

    HttpRequest 注入

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,6 @@
+// 3.匯入 HttpClient 類別
+import { HttpClient } from "@angular/common/http";
+
 import { Component, OnInit } from "@angular/core";
 import { Auth, Api } from "@kkbox/kkbox-js-sdk";
 @Component({
@@ -7,28 +10,41 @@ import { Auth, Api } from "@kkbox/kkbox-js-sdk";
 })
 export class AppComponent implements OnInit {
   title = "app";
+  data;
+
+  // 4.注入 HttpClient 服務
+  constructor(private http: HttpClient) {}
+
   ngOnInit() {
-    var http = require("https");
-    var options = {
-      method: "GET",
-      hostname: "api.kkbox.com",
-      port: null,
-      path: "/v1.1/search?q=剛好遇見你&type=track&territory=TW",
-      headers: {
-        accept: "application/json",
-        authorization: "Bearer zMag02Dzk1a9V9xFqFdtIw=="
-      }
-    };
-    var req = http.request(options, function(res) {
-      var chunks = [];
-      res.on("data", function(chunk) {
-        chunks.push(chunk);
-      });
-      res.on("end", function() {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-      });
+    // 5.發出 Http 要求 (GET)
+    this.http.get<any[]>("https://gank.io/api/random/data/福利/20").subscribe(data => {
+      this.data = data;
     });
-    req.end();
   }
+
+  // ngOnInit() {
+  //   var http = require("https");
+  //   var options = {
+  //     method: "GET",
+  //     hostname: "api.kkbox.com",
+  //     port: null,
+  //     path: "/v1.1/search?q=剛好遇見你&type=track&territory=TW",
+  //     headers: {
+  //       accept: "application/json",
+  //       authorization: "Bearer zMag02Dzk1a9V9xFqFdtIw=="
+  //     }
+  //   };
+  //   var req = http.request(options, function(res) {
+  //     var chunks = [];
+  //     res.on("data", function(chunk) {
+  //       chunks.push(chunk);
+  //     });
+  //     res.on("end", function() {
+  //       var body = Buffer.concat(chunks);
+  //       this.data =  body.toString();
+  //       // console.log(body.toString());
+  //     });
+  //   });
+  //   req.end();
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+// 1.httpModule import 模組物件
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // 2.HttpClient 服務元件封裝在 HttpClientModule 模組
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
