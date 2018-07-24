e8edb01bf5caea7b1f456e0752514a777876089d
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2018-07-24 11:54:32 +0800

    change Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 // 3.匯入 HttpClient 類別
-import { HttpClient } from "@angular/common/http";
-import { HttpHeaders } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http'
+import { HttpHeaders } from '@angular/common/http'
 
-import { Component, OnInit } from "@angular/core";
-import { Auth, Api } from "@kkbox/kkbox-js-sdk";
+import { Component, OnInit } from '@angular/core'
+import { Auth, Api } from '@kkbox/kkbox-js-sdk'
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = "app";
-  data:any;
+  title = 'app'
+  data: any
 
   // 4.注入 HttpClient 服務
   constructor(private http: HttpClient) {}
@@ -19,18 +19,18 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     // 5.發出 Http 要求 (GET)
     let headers = new HttpHeaders({
-      Authorization: "Bearer zMag02Dzk1a9V9xFqFdtIw=="
-    });
-    var limit = 20;
-    var q = "五月天";
-    var offset = 30;
-    var terr = "TW";
-    var type = "track";
-    var dataURL = `https://api.kkbox.com/v1.1/search?limit=${limit}&offset=${offset}&q=${q}&territory=${terr}&type=${type}`;
-    this.http.get<any[]>(dataURL, { headers: headers }).subscribe((data:any) => {
-      this.data = data.tracks.data;
-      console.log(data);
-    });
+      Authorization: 'Bearer UqHZnTkbrNZeH3Bar7uo+g=='
+    })
+    var limit = 20
+    var q = '五月天'
+    var offset = 30
+    var terr = 'TW'
+    var type = 'track'
+    var dataURL = `https://api.kkbox.com/v1.1/search?limit=${limit}&offset=${offset}&q=${q}&territory=${terr}&type=${type}`
+    this.http.get<any[]>(dataURL, { headers: headers }).subscribe((data: any) => {
+      this.data = data.tracks.data
+      console.log(data)
+    })
   }
 
   // ngOnInit() {
